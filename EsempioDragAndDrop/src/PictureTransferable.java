5d0d7f6b413be89a02998b62a75f303972110a16
martemucci.patrizia <martemucci.patrizia@85007930-110c-11df-9b0b-f982558e19cf>
2010-02-03 22:10:26 +0000

    Punto 0 - esempio

---
+++ EsempioDragAndDrop/src/PictureTransferable.java
@@ -0,0 +1,30 @@
+import java.awt.Image;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+
+class PictureTransferable implements Transferable {
+    private Image image;
+    DataFlavor pictureFlavor;
+
+    PictureTransferable(DTPicture pic) {
+      image = pic.image;
+    }
+
+    public Object getTransferData(DataFlavor flavor)
+        throws UnsupportedFlavorException {
+      if (!isDataFlavorSupported(flavor)) {
+        throw new UnsupportedFlavorException(flavor);
+      }
+      return image;
+    }
+
+    public DataFlavor[] getTransferDataFlavors() {
+    	pictureFlavor= DataFlavor.imageFlavor;
+	return new DataFlavor[] { pictureFlavor};
+    }
+
+    public boolean isDataFlavorSupported(DataFlavor flavor) {
+      return pictureFlavor.equals(flavor);
+    }
+  }
