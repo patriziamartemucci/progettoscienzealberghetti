0768811996ec64376004eea2c2977c655b14a7e8
nukeblast34 <nukeblast34@85007930-110c-11df-9b0b-f982558e19cf>
2010-03-17 12:40:26 +0000

    Cosa c'è da dire? Beh tutto funzia bene... ora è da risolvere le carte bianche che sono diverse da quelle piene (ma dai? e invece è che sono proprio di forme diverse). Avevo pensato di fornire anche a loro un'immagine, quella di carta bianca che avevo utilizzato per creare le altre.

---
+++ trunk/NukeBlast34/GiocoCarte/DragAndDropScuola/src/DragPictureDemo.java
@@ -0,0 +1,398 @@
+/* From http://java.sun.com/docs/books/tutorial/index.html */
+
+/*
+ * Copyright (c) 2006 Sun Microsystems, Inc. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * -Redistribution of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *
+ * -Redistribution in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *
+ * Neither the name of Sun Microsystems, Inc. or the names of contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * This software is provided "AS IS," without a warranty of any kind. ALL
+ * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING
+ * ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MIDROSYSTEMS, INC. ("SUN")
+ * AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE
+ * AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL,
+ * INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY
+ * OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE,
+ * EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
+ * You acknowledge that this software is not designed, licensed or intended
+ * for use in the design, construction, operation or maintenance of any
+ * nuclear facility.
+ */
+
+/*
+ * DragPictureDemo.java is a 1.4 example that requires the following files:
+ * Picture.java DTPicture.java PictureTransferHandler.java images/Maya.jpg
+ * images/Anya.jpg images/Laine.jpg images/Cosmo.jpg images/Adele.jpg
+ * images/Alexi.jpg
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class DragPictureDemo extends JPanel implements ActionListener{
+	
+  //Pannelli per scegliere chi muove, cosa e dove disporre le carte.
+	
+  static JPanel MovePanel  = new JPanel(new GridLayout( 1, 3, 0, 0));
+  static JPanel WhatPanel  = new JPanel(new GridLayout( 1, 5, 0, 0));
+  static JPanel TablePanel = new JPanel(new GridLayout( 2, 5, 0, 0));
+  static JPanel TrashPanel = new JPanel(new GridLayout( 1, 1, 0, 0));
+  static JPanel VerifyPanel = new JPanel();
+	
+  //Variabili globali per condividere fra i componenti le dimensioni del frame.
+
+  static int globalwidth = 0 , globalheight = 0 , panelsgap = 0;
+  
+  //Costanti da utilizzare per il calcolo delle varie dimensioni e coordinate di posizione.
+  
+  final static int IMG_X = 96 , IMG_Y = 128 , BUT_Y = 40 , TRSH_X = 80;
+  
+  //Oggetti DTPicture che serviranno nella GUI.
+  
+  DTPicture moveA, moveB, moveC, whatA, whatB, whatC, whatD, whatE, move1, move2,
+            move3, move4, move5, what1, what2, what3, what4, what5, trash;
+  
+  //Bottone per la verifica.
+  
+  JButton verifica;
+
+  //Vettori di immagini usati negli algoritmi di mescolamento e disposizione delle carte.
+  
+  static Image[] vetMove;
+  static Image[] vetMovernd;
+  static Image[] vetWhat;
+  static Image[] vetWhatrnd;
+  
+  //Tre tipi di handler per tre diversi comportamenti delle carte.
+  
+  PictureTransferHandler picHandler;
+  PictureTransferHandler picHandler2;
+  PictureTransferHandler picHandler3;
+
+  //Il costruttore.
+  
+  public DragPictureDemo() {
+	  
+	//Nessun LayoutManager.
+	  
+	setLayout(null);
+
+	//Impostazione degli handler.
+	
+    picHandler = new PictureTransferHandler(-1);
+    picHandler2 = new PictureTransferHandler(1);
+    picHandler3 = new PictureTransferHandler(0);
+    
+    //Attribuzione dei nomi delle immagini di chi muove.
+    
+    vetMove=new Image[3];
+    vetMovernd=new Image[3];
+    vetMove[0]=createImageIcon("muove1.jpg", "muove1").getImage();
+    vetMove[1]=createImageIcon("muove2.jpg", "muove2").getImage();
+    vetMove[2]=createImageIcon("muove3.jpg", "muove3").getImage();
+    
+    //Attribuzione dei nomi delle immagini di cosa è mosso.
+    
+    vetWhat=new Image[5];
+    vetWhatrnd=new Image[5];
+    vetWhat[0]=createImageIcon("cosa1.jpg", "cosa1").getImage();
+    vetWhat[1]=createImageIcon("cosa2.jpg", "cosa2").getImage();
+    vetWhat[2]=createImageIcon("cosa3.jpg", "cosa3").getImage();
+    vetWhat[3]=createImageIcon("cosa4.jpg", "cosa4").getImage();
+    vetWhat[4]=createImageIcon("cosa5.jpg", "cosa5").getImage();
+    
+    //Randomizzazione per chi muove.
+    
+    for(int i=0;i<3;i++) vetMovernd[i]=vetMove[i];
+    for(int i=0;i<100;i++){
+    	int x=(int)(Math.random()*3);
+    	int y=(int)(Math.random()*3);
+    	Image temp=vetMovernd[x];
+    	vetMovernd[x]=vetMovernd[y];
+    	vetMovernd[y]=temp;
+    }
+    
+    //Randomizzazione per cosa è mosso.
+    
+    for(int i=0;i<5;i++) vetWhatrnd[i]=vetWhat[i];
+    for(int i=0;i<100;i++){
+    	int x=(int)(Math.random()*5);
+    	int y=(int)(Math.random()*5);
+    	Image temp=vetWhatrnd[x];
+    	vetWhatrnd[x]=vetWhatrnd[y];
+    	vetWhatrnd[y]=temp;
+    }
+    
+    //Le tre immagini di chi muove.
+    
+    moveA = new DTPicture(vetMovernd[0],true);
+    moveA.setTransferHandler(picHandler);
+    MovePanel.add(moveA);
+    
+    moveB = new DTPicture(vetMovernd[1],true);
+    moveB.setTransferHandler(picHandler);
+    MovePanel.add(moveB);
+    
+    moveC = new DTPicture(vetMovernd[2],true);
+    moveC.setTransferHandler(picHandler);
+    MovePanel.add(moveC);
+    
+    //Le cinque immagini di cosa viene mosso.
+    
+    whatA = new DTPicture(vetWhatrnd[0],true);
+    whatA.setTransferHandler(picHandler);
+    WhatPanel.add(whatA);
+    
+    whatB = new DTPicture(vetWhatrnd[1],true);
+    whatB.setTransferHandler(picHandler);
+    WhatPanel.add(whatB);
+    
+    whatC = new DTPicture(vetWhatrnd[2],true);
+    whatC.setTransferHandler(picHandler);
+    WhatPanel.add(whatC);
+    
+    whatD = new DTPicture(vetWhatrnd[3],true);
+    whatD.setTransferHandler(picHandler);
+    WhatPanel.add(whatD);
+   
+    whatE = new DTPicture(vetWhatrnd[4],true);
+    whatE.setTransferHandler(picHandler);
+    WhatPanel.add(whatE);
+    
+    //I dieci contenitori dove disporre le immagini.
+    
+    move1 = new DTPicture(null,true);
+    move1.setTransferHandler(picHandler2);
+    TablePanel.add(move1);
+    
+    move2 = new DTPicture(null,true);
+    move2.setTransferHandler(picHandler2);
+    TablePanel.add(move2);
+    
+    move3 = new DTPicture(null,true);
+    move3.setTransferHandler(picHandler2);
+    TablePanel.add(move3);
+    
+    move4 = new DTPicture(null,true);
+    move4.setTransferHandler(picHandler2);
+    TablePanel.add(move4);
+    
+    move5 = new DTPicture(null,true);
+    move5.setTransferHandler(picHandler2);
+    TablePanel.add(move5);
+    
+    what1 = new DTPicture(null,true);
+    what1.setTransferHandler(picHandler2);
+    TablePanel.add(what1);
+    
+    what2 = new DTPicture(null,true);
+    what2.setTransferHandler(picHandler2);
+    TablePanel.add(what2);
+    
+    what3 = new DTPicture(null,true);
+    what3.setTransferHandler(picHandler2);
+    TablePanel.add(what3);
+    
+    what4 = new DTPicture(null,true);
+    what4.setTransferHandler(picHandler2);
+    TablePanel.add(what4);
+    
+    what5 = new DTPicture(null,true);
+    what5.setTransferHandler(picHandler2);
+    TablePanel.add(what5);
+    
+    trash = new DTPicture(createImageIcon("trash.jpg", "trash").getImage(),false);
+    trash.setTransferHandler(picHandler3); 
+    TrashPanel.add(trash);
+    
+    //Bottone per la verifica.
+    
+    verifica = new JButton("Verifica");
+    verifica.addActionListener(this);
+    VerifyPanel.add(verifica);
+    
+    //Imposta tutti i jpanel sul jpanel principale.
+    
+    jpanelsUpdate();
+
+    //Aggiunge tutti i jpanel sul jpanel principale.
+    
+    add(MovePanel);
+    add(WhatPanel);
+    add(TablePanel);
+    add(TrashPanel);
+    add(VerifyPanel);
+
+  }
+    
+  //Returns an ImageIcon, or null if the path was invalid.
+  
+  protected static ImageIcon createImageIcon(String path, String description) {
+    java.net.URL imageURL = DragPictureDemo.class.getResource(path);
+    System.out.println("metodo createImageIcon.  Path: "+path+"  --  description: "+description);
+    if (imageURL == null) {
+      System.err.println("Resource not found: " + path);
+      return null;
+    } else {
+      return new ImageIcon(imageURL, description);
+    }
+  }
+
+  /**
+   * Create the GUI and show it. For thread safety, this method should be
+   * invoked from the event-dispatching thread.
+   */
+  
+  private static void createAndShowGUI() {
+
+    //Crea il frame, toglie il LayoutManager e abilità il tasto X per chiudere la finestra.
+	//Dichiarato final perchè se no il listener rompe.
+	  
+    final JFrame frame = new JFrame("Gioco delle carte di chi muove cosa.");
+    frame.setLayout(null);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    //Crea una nuova istanza della GUI e la imposta come pannello del contenuto del frame.
+    
+    DragPictureDemo mainpanel = new DragPictureDemo();
+    mainpanel.setOpaque(true);
+    frame.setContentPane(mainpanel);
+
+    //Impostazioni del frame per visualizzarlo bene.
+    
+    frame.setSize( globalwidth , globalheight );
+    frame.setVisible(true);
+    
+    //Aggiunta del listener per captare l'evento resize, aggiornare le due variabili globali
+    //delle dimensioni e adattare di conseguenza i panelli al suo interno.
+    
+    frame.addComponentListener(new ComponentAdapter() {
+    	
+    	  public void componentResized(ComponentEvent event) {
+    		  
+    		  //Aggiornamento delle variabili globali.
+    		  //Se la nuova dimensione è minore di quella minima utile per disegnare il tutto,
+    		  //allora viene ripristinata quella minima.
+    		  
+    		  globalwidth = Math.max( (int)frame.getSize().getWidth() , IMG_X*5 );
+    		  globalheight = Math.max( (int)frame.getSize().getHeight() , IMG_Y*4 + IMG_X + BUT_Y*2 );
+    		  
+    		  //Aggiornamento delle dimensioni del frame.
+    		  
+    		  frame.setSize( globalwidth , globalheight );
+    		  
+    		  //Chiamata alla funzione che ha come compito di adattare tutti i JPanel.
+    		  
+    		  jpanelsUpdate();
+    		  
+    		  //Ridisegno del frame intero.
+    		  
+    		  frame.setVisible(true);
+    		  
+    	  }
+    });
+  }
+
+  @Override
+  
+  public void actionPerformed(ActionEvent e) {
+	  
+	if(e.getActionCommand().equals("Verifica")){
+		//System.out.println("Hai cliccato sul pulsante verifica");
+		System.out.println("pic1.image.getsource: "+move1.image.getSource());
+
+		int conta=0;
+		//if(move1.image.)
+/*		for(int i=0;i<6;i++){
+			switch (i){
+				case 0:
+					if(whatD.image!=null && vetImage[0].getSource().equals(pic7.image.getSource()))conta++;
+					break;
+					
+				case 1:
+					if(whatE.image!=null && vetImage[1].getSource().equals(pic8.image.getSource()))conta++;
+					break;
+					
+				case 2:
+					if(move1.image!=null && vetImage[2].getSource().equals(pic9.image.getSource()))conta++;
+					break;
+					
+				case 3:
+					if(move2.image!=null && vetImage[3].getSource().equals(pic10.image.getSource()))conta++;
+					break;
+					
+				case 4:
+					if(move3.image!=null && vetImage[4].getSource().equals(pic11.image.getSource()))conta++;
+					break;
+					
+				case 5:
+					if(move4.image!=null && vetImage[5].getSource().equals(pic12.image.getSource()))conta++;
+			}
+			
+		}
+*/
+		if(conta==6){
+			System.out.println("Tutto OK!!!!");
+			JOptionPane.showMessageDialog(null,"Hai vinto!!! ");
+		}
+		else{
+			System.out.println("Riprova!");
+			JOptionPane.showMessageDialog(null,"Hai perso!!! ");
+		}
+	}
+  }
+
+  public static void jpanelsUpdate(){
+	  
+	  panelsgap = (globalheight - IMG_Y*5 - BUT_Y )/7 ;
+	  
+	  //Reimpostazione delle dimensioni di ogni pannello.
+	  
+	  MovePanel.setSize( globalwidth , IMG_Y );
+	  WhatPanel.setSize( globalwidth , IMG_Y );
+	  TablePanel.setSize( globalwidth , IMG_Y*2 + panelsgap );
+	  TrashPanel.setSize( IMG_X , IMG_X );
+	  VerifyPanel.setSize( globalwidth , BUT_Y );
+
+	  //Reimpostazione delle posizioni di ogni pannello.
+	  
+	  WhatPanel.setLocation( 0 , IMG_Y + panelsgap );
+	  TablePanel.setLocation( 0 , IMG_Y*2 + panelsgap*2 );
+	  TrashPanel.setLocation( globalwidth/2 - TRSH_X/2 , IMG_Y*4 + panelsgap*4 );
+	  VerifyPanel.setLocation( 0 , IMG_Y*4 + IMG_X + panelsgap*5 );
+	  
+  }
+  
+  public static void main(String[] args) {
+	
+	//Dimensioni iniziali del frame.  
+	  
+	globalwidth = IMG_X*5;
+	globalheight = IMG_Y*4 + IMG_X + BUT_Y*2;
+	
+    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+      
+    	public void run() {
+    		createAndShowGUI();
+    	}
+    });
+  }
+}
+
+  
---
+++ trunk/NukeBlast34/GiocoCarte/DragAndDropScuola/src/Picture.java
@@ -0,0 +1,96 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.accessibility.Accessible;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+
+/*
+ * Picture.java is used by the 1.4 TrackFocusDemo.java and DragPictureDemo.java
+ * examples.
+ */
+
+class Picture extends JComponent implements MouseListener, FocusListener,
+    Accessible {
+  Image image;
+
+  public Picture(Image image) {
+		
+    this.image = image;
+    //this.setSize(96, 128);
+    setFocusable(true);
+    addMouseListener(this);
+    addFocusListener(this);
+  }
+
+  public void mouseClicked(MouseEvent e) {
+    //Since the user clicked on us, let's get focus!
+    requestFocusInWindow();
+  }
+
+  public void mouseEntered(MouseEvent e) {
+  }
+
+  public void mouseExited(MouseEvent e) {
+  }
+
+  public void mousePressed(MouseEvent e) {
+  }
+
+  public void mouseReleased(MouseEvent e) {
+  }
+
+  public void focusGained(FocusEvent e) {
+    //Draw the component with a red border
+    //indicating that it has focus.
+    this.repaint();
+  }
+
+  public void focusLost(FocusEvent e) {
+    //Draw the component with a black border
+    //indicating that it doesn't have focus.
+    this.repaint();
+  }
+
+  //Returns an ImageIcon, or null if the path was invalid.
+  
+  protected static ImageIcon createImageIcon(String path, String description) {
+    java.net.URL imageURL = DragPictureDemo.class.getResource(path);
+    System.out.println("metodo createImageIcon.  Path: "+path+"  --  description: "+description);
+    if (imageURL == null) {
+      System.err.println("Resource not found: " + path);
+      return null;
+    } else {
+      return new ImageIcon(imageURL, description);
+    }
+  }
+  
+  protected void paintComponent(Graphics graphics) {
+    Graphics g = graphics.create();
+
+    //Draw in our entire space, even if isOpaque is false.
+    g.setColor(Color.WHITE);
+    g.fillRect(0, 0, image == null ? 96 : image.getWidth(this),
+        image == null ? 128 : image.getHeight(this));
+
+    if (image != null) {
+      //Draw image at its natural size of 125x125.
+      g.drawImage(image, 0, 0, this);
+    }
+    
+    //Add a border, red if picture currently has focus
+    if (isFocusOwner()) {
+      g.setColor(Color.RED);
+    } else {
+      g.setColor(Color.BLACK);
+    }
+    g.drawRect(0, 0, image == null ? 125 : image.getWidth(this),
+        image == null ? 125 : image.getHeight(this));
+    g.dispose();
+  }
+}
